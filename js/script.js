b5f8424f788591c80b9a2641cdcfa98194293989
Maykopr <maykopr@hotmail.com>
2022-09-20 09:46:06 -0300

    shadow box

--- js/script.js
+++ js/script.js
@@ -9,12 +9,19 @@ function clean() {
 }
 function backspace() {
     if (currentValue.textContent) {
-        currentValue.innerHTML = currentValue.innerHTML.substring(0, currentValue.innerHTML.length - 1);
+        currentValue.innerHTML = currentValue.innerHTML.slice(0, - 1);
     }
 }
 function result() {
     if (currentValue.textContent != 'erro') {
         currentValue.innerHTML = eval(currentValue.innerHTML)
         previousValue.innerHTML = currentValue.innerHTML;
+    }
+}
+function filter(value) {
+    if (currentValue.innerHTML.includes(value)) {
+        return;
+    } else {
+        insert(value);
     }
 }
