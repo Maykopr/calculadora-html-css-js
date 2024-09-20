d065eb21fa3ac110f765251ce2efd129d7190d3a
Mayko Costa <maykopr@hotmail.com>
2024-09-20 15:57:55 -0300

    style: add prettier e eslint

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Calculadora HMTL/CSS/JS
 
- Esta é uma calculadora simples para treino de HTML e CSS que usa como lógica a função eval() do JS.
- Apesar de simples, possui as seguintes implementações:
+Esta é uma calculadora simples para treino de HTML e CSS que usa como lógica a função eval() do JS.
+Apesar de simples, possui as seguintes implementações:
 
 - Não permite a repetição errada dos operadores e da pontuação.
 - Possui também função try/catch para capturar possíveis erros.
 - Impõe limite de valor digitado.
 - Substitui o último operador digita sem necessidade de apagar.
 
- Outro projeto mais robusto será implementado assim que possível.
+Outro projeto mais robusto será implementado assim que possível.
 
 ![imagem](./img/calculadora-js.jpg)
--- js/script.js
+++ js/script.js
@@ -1,53 +1,50 @@
 const currentValue = document.querySelector(".currentValue");
-const previousValue = document.querySelector('.previousValue');
+const previousValue = document.querySelector(".previousValue");
 
 function insert(value) {
-    if (currentValue.innerHTML.length > 20) {
-        return
-    }
-    currentValue.innerHTML += value;
-
+      if (currentValue.innerHTML.length > 20) {
+            return;
+      }
+      currentValue.innerHTML += value;
 }
 function clean() {
-    currentValue.innerHTML = "";
+      currentValue.innerHTML = "";
 }
 function backspace() {
-    if (currentValue.textContent) {
-        currentValue.innerHTML = currentValue.innerHTML.slice(0, - 1);
-    }
+      if (currentValue.textContent) {
+            currentValue.innerHTML = currentValue.innerHTML.slice(0, -1);
+      }
 }
 function result() {
-    if (currentValue.textContent != 'erro') {
-        tryEval(currentValue.innerHTML);
-        previousValue.innerHTML = currentValue.innerHTML;
-    }
+      if (currentValue.textContent != "erro") {
+            tryEval(currentValue.innerHTML);
+            previousValue.innerHTML = currentValue.innerHTML;
+      }
 }
 function tryEval(e) {
-    try {
-        currentValue.innerHTML = eval(e)
-    } catch { currentValue.innerHTML = "ERRO" }
-
+      try {
+            currentValue.innerHTML = eval(e);
+      } catch {
+            currentValue.innerHTML = "ERRO";
+      }
 }
-
 
 function filterDot(value) {
-    let fator = currentValue.innerHTML.split(new RegExp('[-| +|*| /]'));
-    if (fator[fator.length - 1].includes(value)) {
-        return;
-    } else {
-        insert(value);
-    }
+      let fator = currentValue.innerHTML.split(new RegExp("[-| +|*| /]"));
+      if (fator[fator.length - 1].includes(value)) {
+            return;
+      } else {
+            insert(value);
+      }
 }
 function filterOperator(value) {
-    let lastDigit = currentValue.innerHTML.substring(currentValue.innerHTML.length - 1);
-    if (lastDigit === value) {
-        return;
-    }
-    if (lastDigit == '-' || lastDigit == '+' || lastDigit == '*' || lastDigit == '/') {
-        currentValue.innerHTML = (currentValue.innerHTML.slice(0, - 1) + value);
-    }
-    else {
-        insert(value);
-    }
+      let lastDigit = currentValue.innerHTML.substring(currentValue.innerHTML.length - 1);
+      if (lastDigit === value) {
+            return;
+      }
+      if (lastDigit == "-" || lastDigit == "+" || lastDigit == "*" || lastDigit == "/") {
+            currentValue.innerHTML = currentValue.innerHTML.slice(0, -1) + value;
+      } else {
+            insert(value);
+      }
 }
-
